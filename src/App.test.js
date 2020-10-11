26983ee0e08bdec940939aac50e67ffd152c0ee7
Naina Razafindrabiby <nainarazz@gmail.com>
2020-10-11 11:45:39 +0300

    refactor test in App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,56 +1,52 @@
 import React from 'react';
-import { fireEvent, render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 import mockData from './mockData';
 
+beforeEach(() => {
+  fetchMock.once(
+    [
+      JSON.stringify(mockData)
+    ],
+  )
+})
+
 describe("<App /> test", () => {
-  beforeEach(() => fetchMock.resetMocks())
-  
   it("should render <App /> component", async () => {
-    fetchMock.once(JSON.stringify(mockData))
     render(<App />);
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
     expect(screen.getByText(/My Todo List/i)).toBeInTheDocument();
   });
 
   it("should add a todo item", async () => {
-    fetchMock.mockResponses(
-      [
-        JSON.stringify(mockData)
-      ],
-      [
-        JSON.stringify({
-          userId: 3,
-          id: Math.floor(Math.random() * 100) + 1 ,
-          title: 'Do math homework',
-          completed: false
-        })
-      ]
-    )
+    fetchMock.once(JSON.stringify({
+      userId: 3,
+      id: Math.floor(Math.random() * 100) + 1 ,
+      title: 'Do math homework',
+      completed: false
+    }));
 
     render(<App />);
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
     
-    fireEvent.change(screen.getByRole("textbox"), { target: { value: 'Do math homework'}});
-    fireEvent.submit(screen.getByText(/Add new todo/i));
+    userEvent.type(screen.getByRole("textbox"), 'Do math homework');
+    userEvent.click(screen.getByText(/Add new todo/i));
     await waitForElementToBeRemoved(() => screen.getByText(/saving/i));
     expect(screen.getByText(/Do math homework/i)).toBeInTheDocument();
-
   });
 
   it("remove todo from list", async () => {
-    fetchMock.once(JSON.stringify(mockData))
     render(<App />);
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
-    fireEvent.click(screen.getByTestId('close-btn-3'));
+    userEvent.click(screen.getByTestId('close-btn-3'));
     expect(screen.queryByText(/Take out the trash/i)).not.toBeInTheDocument();
   });
 
   it("todo item should be crossed out after completing", async () => {
-    fetchMock.once(JSON.stringify(mockData))
     render(<App />);
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
-    fireEvent.click(screen.getByTestId('checkbox-1'));
+    userEvent.click(screen.getByTestId('checkbox-1'));
     expect(screen.getByText(/eat breakfast/i)).toHaveClass('completed');
   });
 });
