3019d556ec6f2710a0761625f12f7bf3d3ebd85d
Naina Razafindrabiby <nainarazz@gmail.com>
2020-10-11 11:45:13 +0300

    change test name for todo item

--- src/components/TodoItem/TodoItem.test.js
+++ src/components/TodoItem/TodoItem.test.js
@@ -5,17 +5,15 @@ import TodoItem from './TodoItem';
 import mockData from '../../mockData';
 
 describe('<TodoItem /> tests', () => {
-  it("should render todo item properly", () => {
+  it("should render todo item with close button.", () => {
     render(<TodoItem todo={mockData[0]} />);
-    expect(screen.getByTestId('checkbox-1')).toBeInTheDocument();
     expect(screen.queryByText(/eat breakfast/i)).toBeInTheDocument();
     expect(screen.getByTestId('close-btn-1')).toBeInTheDocument();
   });
   
-  it("should render todo item properly", () => {
+  it("should render todo item with checkbox.", () => {
     render(<TodoItem todo={mockData[0]} />);
     expect(screen.getByTestId('checkbox-1')).toBeInTheDocument();
     expect(screen.queryByText(/eat breakfast/i)).toBeInTheDocument();
-    expect(screen.getByTestId('close-btn-1')).toBeInTheDocument();
   });
 })
